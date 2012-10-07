4a4f5c3764637cb0d25fe7bf888871897817b71c
Ditti4 <ricod@riditt.de>
2012-10-07 16:36:05 +0200

    Adding the messages class

---
+++ messages.php
@@ -0,0 +1,55 @@
+<?php
+/*
+## This file is part of FINOd.
+##
+## FINOd is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## FINOd is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with FINOd.
+## If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class messages
+{
+	public $message, $sender, $channel, $time, $spaceexploded, $colonexploded, $command;
+
+	function __construct($message)
+	{
+		self::$message = $message;
+		self::$spaceexploded = explode(' ', $message);
+		self::$colonexploded = explode(':', $message);
+		self::$time = date(H).':'.date(i).':'.date(s);
+	}
+
+	function getSender()
+	{
+		if (empty($this->sender))
+		{
+			$this->sender = substr($this->message, 1, strpos($this->message, '!'));
+		}
+		return $this->sender;
+	}
+
+	function getChannel()
+	{
+		if (empty($this->channel))
+		{
+			$this->channel = $this->spaceexploded[2];
+		}
+		return $this->channel;
+	}
+
+	function getTime()
+	{
+		return $this->time;
+	}
+
+}
